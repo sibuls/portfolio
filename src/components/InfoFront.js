65bb5f66bfbb0d3de66051109d48449f2a5f5a1e
Sibuls <sebo.orlowski@gmail.com>
2020-11-20 13:11:16 +0100

    laptop initial

--- src/components/InfoFront.js
+++ src/components/InfoFront.js
@@ -4,21 +4,11 @@ const InfoFront = () => {
   return (
     <div>
       <p className='screen-text speech'>
-        Hi all, I'm Seb, late thirty, guy who's passion is projecting everything
-        what can be projected... btw I dont have single furniture bought from
-        shop, all just made by myself.. sorry, office chair has been bought from
-        Ikea.
+        there will be some description of tools: js react babael webpack etc and
+        some graphics
       </p>
 
-      <p className='screen-text speech'>
-        I have worked in several professions in my live - gardener, waiter,
-        miner, restaurant manager. I have always thought that you have to like
-        what you do, especially if we spend most of the day in work. I alwyas
-        wanted to create something thats why I started to learn CSS, Js, React
-        and many others which I didn't even hear about a few years ago. When I
-        got into it I knew that I want to turn it into a full-time job. We'll
-        see what comes out...{' '}
-      </p>
+      <p className='screen-text speech'></p>
     </div>
   );
 };
